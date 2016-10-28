074f7d824267b7ab58703c2e7903237fa77b5c12
Howard Ding <hding2@illinois.edu>
2016-10-28 16:40:36 -0500

    Add code to reset labels directly

--- src/file_counter/gui.clj
+++ src/file_counter/gui.clj
@@ -2,7 +2,7 @@
   (:use seesaw.core
         [seesaw.chooser :only [choose-file]]
         seesaw.mig
-        [seesaw.bind :only [bind property transform]]
+        [seesaw.bind :only [bind property transform notify-later]]
         [file-counter.counter :only [count-root-directory root-directory
                                      directory-count total-file-count
                                      excluded-file-count included-file-count
@@ -15,16 +15,16 @@
 (def directory-button
   (button :text "Choose Directory"))
 (def exclusion-text-area
-  (scrollable (text :multi-line? true
-                    :text ".DS_Store\nThumbs.db"
-                    :rows 10
-                    :columns 40)))
+  (text :multi-line? true
+        :text ".DS_Store\nThumbs.db"
+        :rows 10
+        :columns 40))
 (def run-button
   (button :text "Run"))
-(def directory-count-label "<Dir count>")
-(def total-file-count-label "<Total Files count>")
-(def excluded-file-count-label "<Excluded Files count>")
-(def included-file-count-label "<Valid Files count>")
+(def directory-count-label (label :text "<Dir count>"))
+(def total-file-count-label (label :text "<Total Files count>"))
+(def excluded-file-count-label (label :text "<Excluded Files count>"))
+(def included-file-count-label (label :text "<Valid Files count>"))
 
 ;;;; Wiring
 
@@ -33,21 +33,29 @@
         :action (fn [e] (when-let [new-dir (choose-file directory-button :selection-mode :dirs-only)]
                           (reset! root-directory new-dir))))
 
-(bind directory-count (transform #(.toString %)) (property directory-count-label :text))
-;(bind total-file-count (property total-file-count-label :text))
-;(bind excluded-file-count (property excluded-file-count-label :text))
-;(bind included-file-count (property included-file-count-label :text))
-(bind (property exclusion-text-area :text) (transform #(parse-exclusions %)) exclusions)
-(listen run-button
-        :action (fn [e] (count-root-directory)))
+(bind directory-count directory-count-label)
+(bind total-file-count total-file-count-label)
+(bind excluded-file-count excluded-file-count-label)
+(bind included-file-count included-file-count-label)
+(bind exclusion-text-area (transform #(parse-exclusions %)) exclusions)
 
+;;;I don't know why doing the reset of the atoms inside count-root-directory doesn't reset these because of the bind, but
+;;;it doesn't seem to happen. So we reset the labels manually as part of the process.
+(defn reset-labels []
+  (doseq [label [directory-count-label total-file-count-label excluded-file-count-label included-file-count-label]]
+    (config! label :text "0")))
+
+(listen run-button
+        :action (fn [e]
+                  (reset-labels)
+                  (future (count-root-directory))))
 
 ;;;; Create layout and application
 
 (defn create-layout []
   (mig-panel :items [[directory-button ""]
                      [directory-label "span, wrap"]
-                     [exclusion-text-area "span, wrap"]
+                     [(scrollable exclusion-text-area) "span, wrap"]
                      [run-button "wrap"]
                      ["Directories:" ""]
                      [directory-count-label "wrap"]
@@ -60,6 +68,7 @@
 
 (defn show-gui []
   (native!)
+  (reset! exclusions (parse-exclusions (.getText exclusion-text-area)))
   (invoke-later
     (-> (frame :title "File Counter",
                :content (create-layout),
