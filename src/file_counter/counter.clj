e4624f4787737ef3577bffc1d439a60d3ec42d99
Howard Ding <hding2@illinois.edu>
2016-10-28 21:19:01 -0500

    Fix to skip symbolic links and not to descend into directories that are symbolic links. This is less efficient in a variety of ways, but correct.

--- src/file_counter/counter.clj
+++ src/file_counter/counter.clj
@@ -1,5 +1,6 @@
 (ns file-counter.counter
-  (:use [clojure.string :only [split-lines trim blank?]]))
+  (:use [clojure.string :only [split-lines trim blank?]])
+  (:import (java.nio.file Files LinkOption)))
 
 (def root-directory (atom (java.io.File. ".")))
 (def directory-count (atom 0))
@@ -12,24 +13,30 @@
   (doseq [atom [directory-count total-file-count excluded-file-count included-file-count]]
     (reset! atom 0)))
 
-(defn count-directory []
-  (swap! directory-count inc'))
+(def link-options (into-array LinkOption nil))
 
-(defn count-file [file]
-  (swap! total-file-count inc')
-  (if (contains? @exclusions (.getName file))
-    (swap! excluded-file-count inc')
-    (swap! included-file-count inc')))
+;;;Return the subdirectories to facilitate recursive processing
+(defn process-directory [path]
+  (let [entries (filter #(not (Files/isSymbolicLink %)) (map #(.toPath %) (.listFiles (.toFile path))))
+        directories (filter #(Files/isDirectory % link-options) entries)
+        files (filter #(Files/isRegularFile % link-options) entries)
+        excluded-files (filter #(contains? @exclusions (.toString (.getFileName %))) files)
+        fc (count files)
+        efc (count excluded-files)
+        ifc (- fc efc)]
+    (swap! directory-count #(+ % (count directories)))
+    (swap! total-file-count #(+ % fc))
+    (swap! excluded-file-count #(+ % efc))
+    (swap! included-file-count #(+ % ifc))
+    directories))
 
-(defn count-entry [entry]
-  (if (.isDirectory entry)
-    (count-directory)
-    (count-file entry)))
+(defn count-directories [path-list]
+  (when (not (empty? path-list))
+    (recur `(~@(process-directory (first path-list)) ~@(rest path-list)))))
 
 (defn count-root-directory []
   (reset-counts)
-  (doseq [entry (file-seq @root-directory)]
-    (count-entry entry)))
+  (count-directories (list (.toPath @root-directory))))
 
 (defn parse-exclusions [text]
   (->> text
